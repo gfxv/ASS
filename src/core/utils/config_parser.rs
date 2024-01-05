367352d48a1ba9022974290aaf71315941f19288
gfxv <superfreezu@gmail.com>
2024-01-05 20:37:33 +0300

    feat: config file + parser

---
+++ src/core/utils/config_parser.rs
@@ -0,0 +1,54 @@
+use std::collections::HashMap;
+use std::fmt::format;
+use std::fs::{File, read};
+use std::io::{BufRead, BufReader};
+
+pub struct Parser {
+    file_path: String,
+    data: HashMap<String, String>
+}
+
+impl Parser {
+
+    pub fn new(path: &str) -> Result<Self, String> {
+
+        let file = File::open(&path)
+            .map_err(|err| format!("[FILE.ERROR] Can't open file `{}`\n{}", path, err.to_string()))?;
+        let reader = BufReader::new(file);
+
+        let mut data = HashMap::new();
+
+        for line in reader.lines() {
+            match line {
+                Ok(line) => {
+                    let record = Parser::parse_line(&line);
+                    if record.is_empty()  {
+                        continue;
+                    }
+                    data.insert(
+                        record[0].to_string(),
+                        record[1].to_string()
+                    );
+                }
+                Err(err) => {
+                    return Err(err.to_string());
+                }
+            };
+        }
+
+        Ok(Parser { file_path: path.to_string(), data })
+    }
+
+    fn parse_line(line: &String) -> Vec<String> {
+        if line.starts_with("#") || line.len() == 0 {
+            return Vec::new();
+        }
+        let parts = line.split_once("=").expect("[FILE.ERROR] Some config record(s) is not in `key=value` format");
+
+        Vec::from([String::from(parts.0.trim()), String::from(parts.1.trim())])
+    }
+
+    pub fn get_value(&self, key: &str) -> Result<&String, String> {
+        self.data.get(key).ok_or(format!("[FILE.ERROR] No such record with key `{}`", key))
+    }
+}
---
+++ src/core/utils/file_io.rs
@@ -0,0 +1,34 @@
+use std::env::consts::OS;
+use std::fs::read_to_string;
+use std::path::Path;
+use crate::core::utils::config_parser::Parser;
+
+const CONFIG_PATH: &str = "config.cfg";
+
+const WIN_KEY_PATH: &str = "WIN_KEY_PATH";
+const MAC_KEY_PATH: &str = "MAC_KEY_PATH";
+const LNX_KEY_PATH: &str = "LNX_KEY_PATH";
+
+pub fn get_cypher_key_path() -> Result<String, String> {
+
+    let path: String;
+    let parser = Parser::new(CONFIG_PATH)?;
+
+    match OS {
+        "windows" => path = parser.get_value(&WIN_KEY_PATH)?.to_string(),
+        "macos" => path = parser.get_value(&MAC_KEY_PATH)?.to_string(),
+        "linux" => path = parser.get_value(&LNX_KEY_PATH)?.to_string(),
+        _ => {
+            return Err(String::from("[FILE.ERROR] OS not supported"));
+        }
+    }
+
+    Ok(path)
+}
+
+pub fn get_file_data(path: &String) -> Result<String, String> {
+    let data = read_to_string(path)
+        .map_err(|err| format!("[FILE.ERROR] Can't read file {}\n{}", path, err.to_string()))?;
+
+    Ok(data)
+}
