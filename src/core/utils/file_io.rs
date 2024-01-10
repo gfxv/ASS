8fb633ac69999362e1972da6b97877f416f4fb2c
gfxv <superfreezu@gmail.com>
2024-01-10 04:36:49 +0300

    removed different key path for different OS

--- src/core/utils/file_io.rs
+++ src/core/utils/file_io.rs
@@ -1,29 +1,16 @@
-use std::env::consts::OS;
 use std::fs::read_to_string;
-use std::path::Path;
 use crate::core::utils::config_parser::{
     Parser, CONFIG_PATH
 };
 
 
 
-const WIN_KEY_PATH: &str = "WIN_KEY_PATH";
-const MAC_KEY_PATH: &str = "MAC_KEY_PATH";
-const LNX_KEY_PATH: &str = "LNX_KEY_PATH";
+const KEY_PATH: &str = "KEY_PATH";
 
 pub fn get_cypher_key_path() -> Result<String, String> {
 
-    let path: String;
     let parser = Parser::new(CONFIG_PATH)?;
-
-    match OS {
-        "windows" => path = parser.get_value(&WIN_KEY_PATH)?.to_string(),
-        "macos" => path = parser.get_value(&MAC_KEY_PATH)?.to_string(),
-        "linux" => path = parser.get_value(&LNX_KEY_PATH)?.to_string(),
-        _ => {
-            return Err(String::from("[FILE.ERROR] OS not supported"));
-        }
-    }
+    let path  = parser.get_value(&KEY_PATH)?.to_string();
 
     Ok(path)
 }
